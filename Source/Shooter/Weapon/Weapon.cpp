06535599f77b659c9a8fbbccbab0b04da76aeb40
Snake-420 <114908709+Snake-420@users.noreply.github.com>
2023-11-19 16:03:04 +0000

    Bound Pickup widget to weapon class

--- Source/Shooter/Weapon/Weapon.cpp
+++ Source/Shooter/Weapon/Weapon.cpp
@@ -2,7 +2,9 @@
 
 
 #include "Weapon.h"
-#include"Components/SphereComponent.h"
+#include "Components/SphereComponent.h"
+#include "Components/WidgetComponent.h"
+#include "Shooter/Character/ShooterCharacter.h"
 
 AWeapon::AWeapon()
 {
@@ -21,16 +23,33 @@ AWeapon::AWeapon()
 	AreaSphere->SetupAttachment(RootComponent);
 	AreaSphere->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
 	AreaSphere->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+	PickupWidget = CreateDefaultSubobject<UWidgetComponent>(TEXT("PickupWidget"));
+	PickupWidget->SetupAttachment(RootComponent);
 }
 
 void AWeapon::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	if (PickupWidget)
+	{
+		PickupWidget->SetVisibility(false);
+	}
+
 	if (HasAuthority())
 	{
 		AreaSphere->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
 		AreaSphere->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Overlap);
+		AreaSphere->OnComponentBeginOverlap.AddDynamic(this, &AWeapon::OnSphereOverlap);
+	}
+}
+
+void AWeapon::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	AShooterCharacter* ShooterCharacter = Cast<AShooterCharacter>(OtherActor);
+	if (ShooterCharacter && PickupWidget)
+	{
+		PickupWidget->SetVisibility(true);
 	}
 }
 
