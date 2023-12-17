099907c2ae60024998c6ec9a61b90f0200ac7e76
Snake-420 <viarmurad99@gmail.com>
2023-12-17 19:47:30 +0000

    Created a hit target

--- Source/Shooter/ShooterComponents/CombatComponent.h
+++ Source/Shooter/ShooterComponents/CombatComponent.h
@@ -6,6 +6,8 @@
 #include "Components/ActorComponent.h"
 #include "CombatComponent.generated.h"
 
+#define TRACE_LENGTH 80000.f
+
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class SHOOTER_API UCombatComponent : public UActorComponent
@@ -36,6 +38,8 @@ protected:
 
 	UFUNCTION(NetMulticast, Reliable)
 	void MulticastFire();
+
+	void TraceUnderCrosshairs(FHitResult& TraceHitResult);
 
 private:
 	class AShooterCharacter* Character;
