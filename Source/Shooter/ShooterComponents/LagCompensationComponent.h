8e26a5dac4ad51ba95df12f58f17e9a12bec2c7d
Snake-dot <150725807+Snake-dot@users.noreply.github.com>
2024-10-13 14:12:07 +0100

    Created a function for confirming a hit to tie together the server side rewind function

--- Source/Shooter/ShooterComponents/LagCompensationComponent.h
+++ Source/Shooter/ShooterComponents/LagCompensationComponent.h
@@ -33,6 +33,18 @@ struct FFramePackage
 	TMap<FName, FBoxInformation> HitBoxInfo;
 };
 
+USTRUCT(BlueprintType)
+struct FServerSideRewindResult
+{
+	GENERATED_BODY()
+
+	UPROPERTY()
+	bool bHitConfirmed;
+
+	UPROPERTY()
+	bool bHeadshot;
+};
+
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class SHOOTER_API ULagCompensationComponent : public UActorComponent
 {
@@ -43,7 +55,7 @@ public:
 	friend class AShooterCharacter;
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 	void ShowFramePackage(const FFramePackage& Package, const FColor Color);
-	void ServerSideRewind(
+	FServerSideRewindResult ServerSideRewind(
 		class AShooterCharacter* HitCharacter,
 		const FVector_NetQuantize& TraceStart,
 		const FVector_NetQuantize& HitLocation,
@@ -52,6 +64,15 @@ protected:
 	virtual void BeginPlay() override;
 	void SaveFramePackage(FFramePackage& Package);
 	FFramePackage InterpBetweenFrames(const FFramePackage& OlderFrame, const FFramePackage& YoungerFrame, float HitTime);
+	FServerSideRewindResult ConfirmHit(
+		const FFramePackage& Package, 
+		AShooterCharacter* HitCharacter, 
+		const FVector_NetQuantize& TraceStart, 
+		const FVector_NetQuantize& HitLocation);
+	void CacheBoxPositions(AShooterCharacter* HitCharacter, FFramePackage& OutFramePackage);
+	void MoveBoxes(AShooterCharacter* HitCharacter, const FFramePackage& Package);
+	void ResetHitBoxes(AShooterCharacter* HitCharacter, const FFramePackage& Package);
+	void EnableCharacterMeshCollision(AShooterCharacter* HitCharacter, ECollisionEnabled::Type CollisionEnabled);
 private:
 
 	UPROPERTY()
